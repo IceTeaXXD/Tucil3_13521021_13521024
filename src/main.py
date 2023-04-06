bf38a5c350ce93c9ef896d12bfa404449585b110
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-07 00:22:56 +0700

    feat: update main

--- src/main.py
+++ src/main.py
@@ -3,17 +3,22 @@ from AStar import*
 from UCS import*
 
 G = Graph()
-G.createGraph("test/map2.txt")
+G.createGraph("test/map4.txt")
 G.printGraph()
 start = 1
-goal = 4
+goal = 8
 
 # ASTAR
+print("\nA STAR")
 astar = AStar(start, goal, G.nodes)
 astar.search()
+print("Path: ", end="")
 print(astar.path)
+astar.print_total_cost(astar.path, G.nodes)
 
 # UCS
+print("\nUNIFORM COST SEARCH")
 ucspath = ucs(G, start, goal)
+print("Path: ",end="")
 printList(ucspath)
-print(getCost(ucspath))
+print("Total cost: ",getCost(ucspath))
