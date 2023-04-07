bc12265cea68e81694d1c19ec21067c41a8c60de
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-08 00:15:57 +0700

    feat: add runtime

--- src/main.py
+++ src/main.py
@@ -2,22 +2,30 @@ from Graph import*
 from AStar import*
 from UCS import*
 from Utils import*
+import time
 
 G = Graph()
-G.createGraph("test/map2.txt")
+G.createGraph("test/map5.txt")
 G.printGraph()
 start = 1
-goal = 3
-
+goal = 6
 
 # UCS
 print("\nUNIFORM COST SEARCH")
+startTime = time.perf_counter_ns()
 ucs = UCS(G, start, goal)
+endTime = time.perf_counter_ns()
 print("Path: ", ucs.path)
 print("Total Cost: ", ucs.cost)
+runtime = (endTime - startTime) / 1000
+print("Runtime: {:.2f} ms".format(runtime))
 
 # ASTAR
 print("\nA STAR")
+startTime = time.perf_counter_ns()
 astar = AStar(start, goal, G.nodes)
+endTime = time.perf_counter_ns()
 print("Path: ", astar.path)
 print("Total Cost: ", astar.cost)
+runtime = (endTime - startTime) / 1000
+print("Runtime: {:.2f} ms".format(runtime))
