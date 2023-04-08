c0ff807c3c425dea2e010121a5f7a3c74647a451
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-09 02:04:12 +0700

    fix: euclid to meters

--- src/Utils.py
+++ src/Utils.py
@@ -4,4 +4,4 @@ def list_to_adjacent_pairs(lst) -> list:
     return [(lst[i], lst[i+1]) for i in range(len(lst)-1)]
 
 def euclidean_distance(p1, p2) -> float:
-    return ((((p1[1] - p2[1])**2 + (p1[0] - p2[0])**2)**0.5) *111.322)
+    return ((((p1[1] - p2[1])**2 + (p1[0] - p2[0])**2)**0.5) *111322)
