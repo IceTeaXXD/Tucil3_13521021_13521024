61f8ae7a730282d7df8a32550b35b8f52984fa94
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-09 01:12:08 +0700

    feat: update main for google maps

--- src/main.py
+++ src/main.py
@@ -4,28 +4,40 @@ from UCS import*
 from Utils import*
 import time
 
-G = Graph()
-G.createGraph("test/map5.txt")
-G.printGraph()
-start = 1
-goal = 12
+try:
+    G = Graph()
+    G.createGraph("test/gmap1.txt")
+    G.printGraph()
+    start = 1
+    goal = 11
 
-# UCS
-print("\nUNIFORM COST SEARCH")
-startTime = time.perf_counter_ns()
-ucs = UCS(G, start, goal)
-endTime = time.perf_counter_ns()
-print("Path: ", ucs.path)
-print("Total Cost: ", ucs.cost)
-runtime = (endTime - startTime) / 1000
-print("Runtime: {:.2f} ms".format(runtime))
+    # UCS
+    print("\nUNIFORM COST SEARCH")
+    startTime = time.perf_counter_ns()
+    ucs = UCS(G, start, goal)
+    endTime = time.perf_counter_ns()
+    print("Path: ", ucs.path)
+    print("Total Cost: ", ucs.cost)
+    runtime = (endTime - startTime) / 1000
+    print("Runtime: {:.2f} ms".format(runtime))
 
-# ASTAR
-print("\nA STAR")
-startTime = time.perf_counter_ns()
-astar = AStar(start, goal, G.nodes)
-endTime = time.perf_counter_ns()
-print("Path: ", astar.path)
-print("Total Cost: ", astar.cost)
-runtime = (endTime - startTime) / 1000
-print("Runtime: {:.2f} ms".format(runtime))
+    # ASTAR
+    print("\nA STAR")
+    startTime = time.perf_counter_ns()
+    astar = AStar(start, goal, G.nodes)
+    endTime = time.perf_counter_ns()
+    print("Path: ", astar.path)
+    print("Total Cost: ", astar.cost)
+    runtime = (endTime - startTime) / 1000
+    print("Runtime: {:.2f} ms".format(runtime))
+
+except Exception as e:
+    print(e)
+
+try:
+    G = Graph()
+    G.createGraphWithCoords("test/gmap2.txt")
+    G.printGraph()
+    G.printNodeID()
+except Exception as e:
+    print(e)
