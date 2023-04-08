e563db1a591c9dbc0560de041d51573ca130003d
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-09 02:03:59 +0700

    feat: google maps plotter

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ from AStar import*
 from UCS import*
 from Utils import*
 import time
+import gmplot
 
 try:
     G = Graph()
-    G.createGraph("test/gmap1.txt")
+    G.createGraph("test/map5.txt")
     G.printGraph()
     start = 1
     goal = 11
@@ -38,6 +39,34 @@ try:
     G = Graph()
     G.createGraphWithCoords("test/gmap2.txt")
     G.printGraph()
-    G.printNodeID()
+    start = 1
+    goal = 3
+    ucs = UCS(G, start, goal)
+    print("Path: ", ucs.path)
+    print("Total Cost: ", ucs.cost, "m")
+
+    gmap = gmplot.GoogleMapPlotter(G.Maplat, G.Maplong, 17)
+
+    # give the map a title
+    gmap.title = G.Mapname
+
+    # give description for each node in the map
+    for node in G.nodes:
+        gmap.marker(G.nodeID[node][1], G.nodeID[node][2], 'red', title=f"Node {node} - {G.nodeID[node][0]}")
+
+    # plot the graph, each node is a blue dot, each edge is a blue line
+    for node in G.nodes:
+        for neighbor in G.nodes[node]:
+            gmap.scatter([G.nodeID[node][1], G.nodeID[neighbor][1]], [G.nodeID[node][2], G.nodeID[neighbor][2]], 'red', size = 5, marker = False)
+            gmap.plot([G.nodeID[node][1], G.nodeID[neighbor][1]], [G.nodeID[node][2], G.nodeID[neighbor][2]], 'blue', edge_width=1)
+
+    # plot the path
+    for i in range(len(ucs.path)-1):
+        gmap.plot([G.nodeID[ucs.path[i]][1], G.nodeID[ucs.path[i+1]][1]], [G.nodeID[ucs.path[i]][2], G.nodeID[ucs.path[i+1]][2]], 'green', edge_width=2)
+
+
+    gmap.draw("result/gmap2.html")
+    print("Map saved to result/gmap2.html")
+
 except Exception as e:
     print(e)
