31754be58c2672bcaa9773eaa21db59bc45054fe
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-09 01:11:39 +0700

    feat: add euclidean distance

--- src/Utils.py
+++ src/Utils.py
@@ -1,2 +1,7 @@
+import math
+
 def list_to_adjacent_pairs(lst) -> list:
     return [(lst[i], lst[i+1]) for i in range(len(lst)-1)]
+
+def euclidean_distance(p1, p2) -> float:
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
